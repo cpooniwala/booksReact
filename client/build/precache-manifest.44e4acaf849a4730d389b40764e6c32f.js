109f7c74e9e30fc0045d5524013121ef32bcd5a5
cpooniwala <cpooniwala@gmail.com>
2020-05-09 12:59:57 -0500

    test

---
+++ client/build/precache-manifest.44e4acaf849a4730d389b40764e6c32f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "67f88fdb1311e1f688c9",
+    "url": "/static/css/main.b281769e.chunk.css"
+  },
+  {
+    "revision": "67f88fdb1311e1f688c9",
+    "url": "/static/js/main.e96d31f6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a0d5c17320b3994529e0",
+    "url": "/static/js/2.998b6ee3.chunk.js"
+  },
+  {
+    "revision": "1a1941f78e6f2c2eaf77daac8fb8101d",
+    "url": "/index.html"
+  }
+];
